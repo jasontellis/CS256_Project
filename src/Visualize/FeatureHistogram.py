a75f59f9d02a4300525159c346f4a5990afe0150
jasontellis <jason.tellis91@gmail.com>
2017-12-04 18:16:52 -0800

    Minor changes to Like Dislike Button

--- src/Visualize/FeatureHistogram.py
+++ src/Visualize/FeatureHistogram.py
@@ -16,23 +16,46 @@ print Constants.CLASS_LABELS
 class FeatureHistogram:
 
 
-	def __init__(self, imageVectorList):
+	def __init__(self, imageVectorList, title ='Feature Distribution for Good Images'):
 		colLabels = Constants.FEATURE_LABELS
-		subPlotColumns = 3
+		subPlotColumns = 1
 		subPlotRows = len(colLabels) / subPlotColumns + len(colLabels) % subPlotColumns
 
-		data = pandas.DataFrame.from_records(imageVectorList,columns = Constants.FEATURE_LABELS )#, index = Constants.FEATURE_LABELS)
-		print data.describe()
-		#ling
-		axs=data.plot(kind = 'hist', subplots = True, layout = (4, 2), sharex = False, sharey = False)
-		#ling
+		dataFrame = pandas.DataFrame.from_records(imageVectorList,columns = Constants.FEATURE_LABELS )#, index = Constants.FEATURE_LABELS)
+
+		print 'Model: ',dataFrame.describe()
+		rowCount, colCount = dataFrame.shape
+
+
+		xlimits = []
+		ylimits = []
+		ymin = 0
+		ymax = rowCount
+
+
+		axes = dataFrame.plot(kind = 'hist',
+		                      subplots = True,
+		                      layout = (4, 2),
+		                      sharex = False,
+		                      sharey = False,
+		                      title = title)
+
+		for colIndex in range(colCount):
+			col = dataFrame.iloc[:, colIndex]
+			xmin = min(col)
+			xmax = max(col)
+			print colIndex/2, colIndex%2
+			axis = axes[colIndex/2][colIndex%2]
+			axis.set_xlim(xmin,xmax)
+			axis.set_ylim(ymin,ymax)
+
 
 if __name__ == "__main__":
 	imageVectorList_good, imageLabelList_good = TrainingFileReader.extractTrainingData("../data/training/ling", False, 1)
-	fh_good = FeatureHistogram(imageVectorList_good)
+	fh_good = FeatureHistogram(imageVectorList_good, title = 'Feature Distribution for Good Images')
 	#fig.suptitle('Good image', fontsize = 12)
-	#fig.savefig("../good_feature_histogram.png")
+	# fig.savefig("../good_feature_histogram.png")
 	imageVectorList_bad, imageLabelList_bad = TrainingFileReader.extractTrainingData("../data/training/ling", False, 0)
-	fh_bad = FeatureHistogram(imageVectorList_bad)
-	#fig.suptitle('Bad image', fontsize = 12)
-	#fig.savefig('../bad_feature_histogram.png')
+	fh_bad = FeatureHistogram(imageVectorList_bad, title = 'Feature Distribution for Bad Images')
+	# #fig.suptitle('Bad image', fontsize = 12)
+	# fig.savefig('../bad_feature_histogram.png')
